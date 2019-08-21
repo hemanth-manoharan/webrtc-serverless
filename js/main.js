1ccdec4f790751259d6036725f93c707f70107de
Hemanth M <hemanth.aero@gmail.com>
2019-08-21 20:46:30 +0530

    Correcting peerjs domain missed out in merge

--- js/main.js
+++ js/main.js
@@ -241,7 +241,7 @@ app.appView = new app.ChatListView({collection: app.chatList});
 let peerJSMode = 'remote';
 
 // Default is remote
-let peerJSHost = 'safe-eyrie-39067.herokuapp.com';
+let peerJSHost = 'glacial-temple-29018.herokuapp.com';
 let peerJSPort = 443;
 let peerJSSecure = 'true';
 
